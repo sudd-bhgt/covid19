8d41d49becd55d155e1aeb4e32348cefc461dc43
sudd-bhgt <36042730+sudd-bhgt@users.noreply.github.com>
2020-04-26 19:03:51 +0530

    Add files via upload

---
+++ precache-manifest.e2c9d5cfc9e0b618582f85b6d4b33250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4533b1a954134b5d17f775560c142cb5",
+    "url": "/covid19bihar/index.html"
+  },
+  {
+    "revision": "78b8dba87c48b23efb6a",
+    "url": "/covid19bihar/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "74c16727dcbfeb8ae666",
+    "url": "/covid19bihar/static/css/main.f0ed1b87.chunk.css"
+  },
+  {
+    "revision": "78b8dba87c48b23efb6a",
+    "url": "/covid19bihar/static/js/2.20197cc7.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19bihar/static/js/2.20197cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c16727dcbfeb8ae666",
+    "url": "/covid19bihar/static/js/main.cf91800e.chunk.js"
+  },
+  {
+    "revision": "e1fa400f46031b052207",
+    "url": "/covid19bihar/static/js/runtime-main.831b40ae.js"
+  }
+]);
